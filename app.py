d30f2bf87843c87a33c08387a9eb0e8f570639a4
Facu4994 <alumno29.diaz.grassini.facundo@ipm.edu.ar>
2026-08-19 11:48:09 -0300

    Cambios 19/8 DÍAZ

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('juego.html')
+@app.route('/video_feed')
+def video_feed():
+  ...
+
+
+@app.route('/play', methods=['POST'])
+def play():
+  ...
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
